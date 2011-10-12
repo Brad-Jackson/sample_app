9ba7e9981875b9341f5243a8c5980659ed793fd0
Brad-Jackson <bjsmail@optusnet.com.au>
2011-10-12 14:48:20 +1000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
   root :to => 'pages#home'
 end
